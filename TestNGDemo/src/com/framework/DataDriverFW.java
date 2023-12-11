ebb95933a809e68599f1524027dbab736252cf5c
jigarthakkar123 <jigar.thakkar.tops@gmail.com>
2023-12-11 18:20:25 +0530

    11-12-23 New

--- TestNGDemo/src/com/framework/DataDriverFW.java
+++ TestNGDemo/src/com/framework/DataDriverFW.java
@@ -12,27 +12,28 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class DataDriverFW {
 
-	@Test
-	public String[][] readExcel() throws InvalidFormatException, IOException
+	@DataProvider(name = "jigar")
+	public Object[][] readExcel() throws InvalidFormatException, IOException
 	{
-		String data[][]=null;
+		Object data[][]=null;
 		String filePath="E:\\Automation Tools\\data.xlsx";
 		File file=new File(filePath);
 		XSSFWorkbook workbook=new XSSFWorkbook(file);
-		Sheet sheet=workbook.getSheet("Tops1");
+		Sheet sheet=workbook.getSheet("Tops3");
 		int rows=sheet.getPhysicalNumberOfRows();
 		System.out.println("Total Number Of Rows : "+rows);
-		data=new String[rows][];
+		data=new Object[rows][];
 		for(int i=0;i<data.length;i++)
 		{
 			Row row=sheet.getRow(i);
 			int cols=row.getPhysicalNumberOfCells();
 			System.out.println("Total Number Of Columns : "+cols);
-			data[i]=new String[cols];
+			data[i]=new Object[cols];
 			for(int j=0;j<data[i].length;j++)
 			{
 				Cell cell=row.getCell(j);
@@ -43,26 +44,41 @@ public class DataDriverFW {
 		return data;
 	}
 	WebDriver driver=null;
-	@Test
-	public void test() throws InterruptedException, InvalidFormatException, IOException
+	@Test(dataProvider = "jigar")
+	public void test(String keyword,String data) throws InterruptedException, InvalidFormatException, IOException
 	{
 		System.setProperty("webdriver.chrome.driver", "E:\\Automation Tools\\chromedriver.exe");
 		
-		String data[][]=readExcel();
-		for(int i=0;i<data.length;i++)
+		if(keyword.equals("open browser"))
 		{
 			driver = new ChromeDriver();
-			driver.get("https://www.saucedemo.com/");
+			Thread.sleep(2000);
+		}
+		else if(keyword.equals("enter url"))
+		{
+			driver.get(data);
 			Thread.sleep(2000);
 			driver.manage().window().maximize();
 			Thread.sleep(2000);
-			driver.findElement(By.id("user-name")).sendKeys(data[i][0]);
+		}
+		else if(keyword.equals("enter username"))
+		{
+			driver.findElement(By.id("user-name")).sendKeys(data);
 			Thread.sleep(2000);
-			driver.findElement(By.id("password")).sendKeys(data[i][1]);
+		}
+		else if(keyword.equals("enter password"))
+		{
+			driver.findElement(By.id("password")).sendKeys(data);
 			Thread.sleep(2000);
+		}
+		else if(keyword.equals("click login"))
+		{
 			driver.findElement(By.id("login-button")).click();
 			Thread.sleep(3000);
-			driver.close();
 		}
+		else if(keyword.equals("close browser"))
+		{
+			driver.close();
+		}		
 	}
 }
